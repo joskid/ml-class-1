dc8e4731f95612abca4a8d4339194830c2ab3bf1
Kliment Mamykin <kmamyk@yahoo.com>
2011-10-13 17:55:13 -0400

    Linear regression for one variable

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,11 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    gradient = X' * (X * theta - y) / m;
 
-
-
-
-
+    theta = theta - alpha * gradient;
 
     % ============================================================
 
