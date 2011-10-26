45ec6ed2c6f72cc1676e05b6bd69cef24f99b439
Kliment Mamykin <kmamyk@yahoo.com>
2011-10-26 18:16:51 -0400

    Submitted sigmoid and costFunction for ex2

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -19,13 +19,15 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h = sigmoid(X * theta);
 
+for i = 1:m
+  J = J + -1*y(i)*log(h(i)) - (1- y(i))*log(1 - h(i));
+end
 
+J = J/m;
 
-
-
-
-
+grad = X'*(h - y)./m;
 
 % =============================================================
 
